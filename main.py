8053d95f83907d64fef92985e28e0f8f39f5ca16
avimitin <avimitin@gmail.com>
2020-07-11 13:39:30 +0800

    将请求部分语句删除, 重新构造输出

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
-import requests
-from bs4 import BeautifulSoup
-import re
 import argparse
+from module.request_info import get_ex_info
+from module import percolator
 
-def get_ex_info(url):
-    if re.match('(.*)ex', url):
-        url = re.sub('x', '-', url, 1)
-        print(url)
 
-    html_page = requests.get(url)
-    if html_page.status_code == 200:
-
-        soup = BeautifulSoup(html_page.text, "lxml")
-        
-        info = {}
-
-        info["title"] = soup.find("h1", id = "gj").string
-        info["favcount"] = soup.find("td", id = "favcount").string
-        info["rating"] = soup.find("td", id = "rating_label").string
-        info["artist"] = soup.find("a", href = re.compile('artist')).string
-        
-        return info
-    else:
-        return 'Unfound Page! Status:%s' % html_page.status_code
-
- 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='get URL')
     parser.add_argument('-u',type=str,default=None)
     arg = parser.parse_args()
     url = arg.u
     if url:
-        print(get_ex_info(url))
+        response = get_ex_info(url)
+        comic_info = percolator.filter(response)
+        print('''
+======================
+Title: %s
+Artist: %s
+Category: %s
+Rating: %s
+Tags: %s
+Thumb_URL: %s
+======================
+        '''
+        % (
+            comic_info.title, 
+            comic_info.artist,
+            comic_info.category,
+            comic_info.rating,
+            comic_info.tags,
+            comic_info.thumb
+        )
+        )
     else:
         print("Need An URL!!\nUsage: -u 'https://example.com'")
 
